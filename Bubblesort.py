bc12517ccb4f315499a78ab82592170e9ec1b188
balli <balli@DESKTOP-3FB7QHB>
2022-02-18 20:54:11 -0500

    Accending ✔️Decending ✔️

--- Bubblesort.py
+++ Bubblesort.py
@@ -1,8 +1,10 @@
 list1 = []
-listamm = 5
+listamm = 10
+acc_dec = input("Order numbers up or down:")
+
 
 for k in range(listamm):
-    data = int(input("Enter 5 numbers:"))
+    data = int(input("Enter 10 numbers:"))
     list1.append(data)
 
 def sort(x):
@@ -14,5 +16,19 @@ def sort(x):
                 x[i]=x[j]
                 x[j]=l1
 
-sort(list1)
-print(list1)
+def sort_down(x):
+    l=len(x)
+    for i in range(l):
+        for j in range((i+1),l):
+            if x[i]<x[j]:
+                l1=x[i]
+                x[i]=x[j]
+                x[j]=l1
+
+if acc_dec == "up":
+    sort(list1)
+    print(list1)
+
+if acc_dec == "down":
+    sort_down(list1)
+    print(list1)
